4d5b925447d3e2cec3eb691f833051cb32c97182
Oleksandr Nychyk <nychyk.oleksandr@lll.kpi.ua>
2024-06-03 10:15:59 +0300

    stable vers

--- app/front/static/scripts/script.js
+++ app/front/static/scripts/script.js
@@ -10,7 +10,6 @@ window.addEventListener("scroll", function () {
 });
 
 const token = localStorage.getItem("token");
-console.log(token);
 
 function handleClick(e) {
   e.preventDefault();
@@ -36,4 +35,20 @@ const itemCreateRent = document.querySelector(".menu-item-create-rent");
 
 catalogue.addEventListener("click", handleClick);
 itemRent.addEventListener("click", handleClick);
-itemCreateRent.addEventListener("click", handleClickLink);
+itemCreateRent.addEventListener("click", itemClick);
+
+// console.log("qweekrbj");
+
+// const flatsItem = document.querySelector(".flats-item");
+// console.log(flatsItem);
+// flatsItem.addEventListener("click", itemClick);
+
+// function itemClick() {
+//   console.log("qwe");
+//   const res = confirm("Орендувати цю квартиру?");
+//   if (res) {
+//     alert("Заявка подана успішно!");
+//   } else {
+//     return;
+//   }
+// }
--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ templates = Jinja2Templates(directory="app/front/html")
 
 origins = [
     "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:8000/"
 ]
 
 app.add_middleware(
